31bb1068fb347c5d099fd308a28ce86b058856a0
Sathvik Kanna <k.sathvik2003@gmail.com>
2025-03-21 14:11:48 +0530

    Update sub_node.py

--- ws/src/atvpi/atvpi/sub_node.py
+++ ws/src/atvpi/atvpi/sub_node.py
@@ -62,7 +62,7 @@ class JoystickSubscriber(Node):
         servo_range = 160/180*math.pi # need to calculate, in rad
 
         if(self.mode == 0): # ackermann
-            steer = self.axes[2] 
+            steer = self.axes[3] 
             throttle = -self.axes[1]*2000
             
             if (steer == 0): # no steer input
